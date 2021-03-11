7f0bced28bf1884744f08690bb457eaa7a5d2b48
BiancaDavey <biancamdavey@icloud.com>
2021-03-11 23:47:30 +0100

    Reorganising function calls

--- Transactions.kt
+++ Transactions.kt
@@ -1,34 +1,25 @@
-//class Transactions {
-
-    fun main(args: Array<String>)
+fun main(args: Array<String>)
     {
 
-        // Creating class objects from the CompanyAccount and SavingsAccount subclasses.
+    // Creating class objects from the CompanyAccount and SavingsAccount subclasses.
 
-        val account1 = CompanyAccount(20000.05, "Distributed Holdings Inc");
-        val account2 = SavingsAccount(1050.75, "Bob", "Smith");
+    val account1 = CompanyAccount(20000.05, "Distributed Holdings Inc");
+    val account2 = SavingsAccount(1050.75, "Bob", "Smith");
 
-        // Demonstrating class methods.
+    // Demonstrating class methods.
 
-        account1.displayTransactions();
-        account2.displayTransactions();
+    println("Object details before calling methods: ");
+    account1.displayBalance();
+    account2.displayBalance();
 
-        println("Object details before calling methods: ");
-        account1.displayBalance();
-        account2.displayBalance();
-        account1.displayTransactions();
-        account2.displayTransactions();
+    account1.withdraw(260.00);
+    account2.deposit(3371.65);
+    account2.transfer(2000.00);
 
-        account1.withdraw(260.00);
-        account2.deposit(3371.65);
-        account2.transfer(2000.00);
-
-        println("Object details after calling methods: ");
-        account1.displayBalance();
-        account2.displayBalance();
-        account1.displayTransactions();
-        account2.displayTransactions();
+    println("Object details after calling methods: ");
+    account1.displayBalance();
+    account1.displayTransactions();
+    account2.displayBalance();
+    account2.displayTransactions();
 
     }
-
-//}
