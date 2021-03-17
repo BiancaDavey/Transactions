5877a8a446f273dfc734796ab005976661509918
BiancaDavey <biancamdavey@icloud.com>
2021-03-17 16:34:50 +0100

    Updating superclass constructor and functions.

--- Account.kt
+++ Account.kt
@@ -3,45 +3,48 @@
     The Account class enables the creation of Account objects representing bank accounts.
 */
 
-open class Account(accountBalance : Double) 
+open class Account(accountBalance : Double)
 {
-    var accountBalance : Double = 0.0;
-    var totalWithdrawals : Int = 0;
-    var totalDeposits : Int = 0;
-    var totalTransfers : Int = 0;
 
-    /*
-    TODO: ensure constructor form is correctly implemented.
-    constructor(accountBalance: Double) {
-        accountBalance = balance;
+    // init function is executed when an instance of the class is created.
+    init {
+        println("A new account has been created. ");
     }
-    */
+
+    // open var accountBalance : Double = 0.0
+    open var balance : Double = accountBalance;
+    open var totalWithdrawals : Int = 0
+    open var totalDeposits : Int = 0
+    open var totalTransfers : Int = 0
 
     // Method to make a transaction being a withdrawal from an account object.
 
     open fun withdraw(withdrawAmount : Double) {
-        accountBalance -= withdrawAmount;
+        balance -= withdrawAmount;
         totalWithdrawals++;
+        println("Withdrawing $withdrawAmount from account.");
     }
 
     // Method to make a transaction being a deposit into an account object.
 
     open fun deposit(depositAmount : Double) {
-        accountBalance += depositAmount;
+        balance += depositAmount;
         totalDeposits++;
+        println("Depositing $depositAmount into account.");
     }
 
     // Method to make a transaction being a transfer from an account object.
 
     open fun transfer(transferAmount : Double) {
-        accountBalance -= transferAmount;
+        balance -= transferAmount;
         totalTransfers++;
+        println("Transferring $transferAmount from account.");
     }
 
     // Method to display the balance details of an account.
 
-    open fun displayBalance() {
-        println("Account balance: $accountBalance");
+    fun displayBalance() {
+        println("Account balance: $balance"); // $accountBalance");
     }
 
     // Method to display the record of transactions made by an account.
