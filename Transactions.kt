f3f2007702e94d868b6d61fc7606b3bc5b3c48d2
BiancaDavey <biancamdavey@icloud.com>
2021-03-17 16:35:25 +0100

    Demonstration class print display formatting

--- Transactions.kt
+++ Transactions.kt
@@ -1,25 +1,31 @@
 fun main(args: Array<String>)
     {
 
-    // Creating class objects from the CompanyAccount and SavingsAccount subclasses.
+        // Creating class objects.
 
-    val account1 = CompanyAccount(20000.05, "Distributed Holdings Inc");
-    val account2 = SavingsAccount(1050.75, "Bob", "Smith");
+        var account1 = CompanyAccount(20000.05, "Distributed Holdings Inc");
+        var account2 = SavingsAccount(1050.75, "Bob", "Smith");
 
-    // Demonstrating class methods.
+        // Demonstrating class methods.
 
-    println("Object details before calling methods: ");
-    account1.displayBalance();
-    account2.displayBalance();
+        println("\n Object details before calling methods: \n");
+        println("Account 1 Balance: ");
+        account1.displayBalance();
+        println("Account 2 Balance: ");
+        account2.displayBalance();
 
-    account1.withdraw(260.00);
-    account2.deposit(3371.65);
-    account2.transfer(2000.00);
+        account1.withdraw(260.00);
+        account2.deposit(3371.65);
+        account2.transfer(2000.00);
 
-    println("Object details after calling methods: ");
-    account1.displayBalance();
-    account1.displayTransactions();
-    account2.displayBalance();
-    account2.displayTransactions();
+        println("\n Object details after calling methods: \n");
+        println("Account 1 Balance: ");
+        account1.displayBalance();
+        println("Account 1 Transactions: ");
+        account1.displayTransactions();
+        println("Account 2 Balance: ");
+        account2.displayBalance();
+        println("Account 2 Transactions: ");
+        account2.displayTransactions();
 
-    }
+};
